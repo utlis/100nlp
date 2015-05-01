ab7bfc63517b602a30010faed9fc002d9a6d18f9
Gascar-ShunT <gascarys-shunt@yahoo.co.jp>
2015-05-01 22:03:22 +0900

    rename files in chapter01
    make it possible that other files can import them

--- 2015/chapter01/chp01_06.py
+++ 2015/chapter01/chp01_06.py
@@ -1,7 +1,7 @@
 # 06. 集合
 # "paraparaparadise"と"paragraph"に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，XとYの和集合，積集合，差集合を求めよ．さらに，'se'というbi-gramがXおよびYに含まれるかどうかを調べよ．
 
-import chp1_05 as p05
+import chp01_05 as p05
 
 X = set(p05.n_gramizer("paraparaparadise", 2, unit="char"))
 Y = set(p05.n_gramizer("paragraph", 2, unit="char"))
