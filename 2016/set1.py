3dd77b50815c861dfd39e0a8d9d227da18f35cdd
gardenchild <komazawakazuyo@gmail.com>
2016-06-08 11:36:12 +0900

    Update set1.py

--- 2016/set1.py
+++ 2016/set1.py
@@ -3,3 +3,8 @@ moji=list("stressed")
 #string="stressed"
 moji.reverse()
 print("".join(moji))
+#パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ
+#coding=utf_8
+str = u"パタトクカシーー"
+#str[::2]
+print(str[::2])
