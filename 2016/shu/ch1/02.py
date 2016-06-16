c0cb5c13d0b85c9fe5a1ac4db953ab8e7954bfed
shinchu <cougousei@gmail.com>
2016-06-16 14:14:06 +0900

    list comprehension

--- 2016/shu/ch1/02.py
+++ 2016/shu/ch1/02.py
@@ -9,8 +9,10 @@ str2 = u'タクシー'
 str3 = u''
 
 # 同じ長さの文字列なのでzip()が使える．
-
 for a, b in zip(str1, str2):
     str3 += a + b
-
 print str3.encode('utf-8')
+
+# 内包表記
+list = [x+y for (x, y) in zip(str1, str2)]
+print ''.join(list).encode('utf-8')
